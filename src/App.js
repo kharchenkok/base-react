b54298cc5d505ec0bf75abbbacd83203fc7c0ba4
kharchenkok <k.v.kharchenko@ukr.net>
2020-09-06 19:07:31 +0300

    create basic react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <Fragment>
+//        {/* <Profile {...users} />  */}
+
+//     </Fragment>
+//   )
+// }
+
+// export default App;
